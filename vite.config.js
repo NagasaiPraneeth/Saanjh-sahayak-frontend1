0f81b79a73279bda7f4c3a2258ac2d11749e5516
Naga sai praneeth <123751071+NagasaiPraneeth@users.noreply.github.com>
2024-08-13 03:13:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../backend/build',
+    outDir: 'dist', // Change this to 'dist' for Vercel compatibility
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
       output: {
